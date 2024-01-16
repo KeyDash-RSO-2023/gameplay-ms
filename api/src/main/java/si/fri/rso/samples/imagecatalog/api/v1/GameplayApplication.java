4a3e93e1513f0a5a1124683e52be0f59dedef572
Kurtaga <ak84795@student.uni-lj.si>
2024-01-16 11:29:36 +0100

    Testing base-url

--- api/src/main/java/si/fri/rso/samples/imagecatalog/api/v1/GameplayApplication.java
+++ api/src/main/java/si/fri/rso/samples/imagecatalog/api/v1/GameplayApplication.java
@@ -13,7 +13,7 @@ import org.eclipse.microprofile.openapi.annotations.servers.Server;
 @OpenAPIDefinition(info = @Info(title = "Gameplay API", version = "v1",
         contact = @Contact(email = "ak84795@student.uni-lj.si"),
         license = @License(name = "dev"), description = "API for managing image metadata."),
-        servers = @Server(url = "http://http://20.240.34.248/gameplay"))
+        servers = @Server(url = "http://20.240.34.248/gameplay"))
 @ApplicationPath("/v1")
 public class GameplayApplication extends Application {
 
